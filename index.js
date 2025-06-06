f28ee4746a68e222729ae3441a542c0c4b304651
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2025-06-06 17:55:33 -0600

    add image random changes

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 let randomNum1 = 0
+let randomNum2 = 0
 
-randomNum1 = Math.floor(Math.random() * 6)
+randomNum1 = Math.floor(Math.random() * 6 + 1)
+randomNum2 = Math.floor(Math.random() * 6 + 1)
 
-console.log(randomNum1)
+let dice1 = document.querySelector(".dice-one")
+dice1.setAttribute("src", `./images/dice${randomNum1}.png`)
+
+let dice2 = document.querySelector(".dice-two")
+dice1.setAttribute("src", `./images/dice${randomNum2}.png`)
