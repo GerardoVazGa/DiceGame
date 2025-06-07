7b845c0fa90f90ed408b00bd3e1a2c6a8a0bb8b1
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2025-06-06 18:06:53 -0600

    add win condition on the game

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let randomNum1 = 0
 let randomNum2 = 0
+let principalTitle = document.querySelector(".game-intruction")
 
 randomNum1 = Math.floor(Math.random() * 6 + 1)
 randomNum2 = Math.floor(Math.random() * 6 + 1)
@@ -9,3 +10,11 @@ dice1.setAttribute("src", `./images/dice${randomNum1}.png`)
 
 let dice2 = document.querySelector(".dice-two")
 dice2.setAttribute("src", `./images/dice${randomNum2}.png`)
+
+if(randomNum1 === randomNum2){
+    principalTitle.textContent = "🚩 DRAW! 🚩"
+} else if(randomNum1 < randomNum2){
+    principalTitle.textContent = "PLAYER 2 WINS! 🚩"
+} else{
+    principalTitle.textContent = "🚩 PLAYER 1 WINS!"
+}
